75d89d325197adfc808e470ddf424c201d8399ef
admin <admin@163.com>
2018-09-29 09:37:26 +0800

    完成了账号管理页面的管理列表的账号编辑，登陆页面，退出系统，coo kies,过程中遇到几次语法错误，花费很多时间

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,55 @@ connection.connect(() => {
   console.log('数据连接成功')
 });
 
+// 检查用户名和密码是否正确
+router.post('/userLogin', (req, res) => {
+  let { username, password } = req.body;
+  // 构造sql语句
+  const sqlStr = `select * from user where username='${username}' and password='${password}'`;
+  // 执行sql语句
+  // 执行sql语句（查询用户名和密码是否存在）
+  connection.query(sqlStr, (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      // 如果存在 返回成功的数据对象
+      if (data.length) {
+        // 登录成功 设置cookie (在res.send()之前设置)
+        res.cookie('username', data[0].username);
+        res.cookie('password', data[0].password);
+        res.cookie('groups', data[0].groups);
+
+        res.send({"errcode":1, "msg":"恭喜你，登录成功！"})
+      } else {
+        // 否则 返回失败的数据对象
+        res.send({"errcode":0, "msg":"请检查用户名或密码！"})
+      }
+    }
+  })
+});
+
+// 检验用户是否已经登录
+router.get('/userLogin',(req,res) => {
+  let username = req.cookies.username;
+  if(username){
+    res.send("")
+  }else{
+    res.send('alert("请登录！！！");location.href="./login.html";')
+  }
+});
+
+/* 退出登录 */
+router.get('/logout', (req, res) => {
+  // 清除cookie
+  res.clearCookie('username');
+  res.clearCookie('password');
+  res.clearCookie('groups');
+
+  // 弹出对应提示 跳转到登录页面
+  res.send('<script>alert("退出成功！"); location.href="http://localhost:888/login.html";</script>')
+
+})
+
 // 添加账号功能
 // 接收前端发送的请求
 router.post('/userAdd', (req, res) => {
@@ -44,28 +93,85 @@ router.get('/userList', (req, res) => {
       res.send(data)
     }
   })
-})
+});
 // 管理列表中的删除数据
 // 接收前端发来的请求，删除一条用户数据
 router.get('/userDelete', (req, res) => {
   // 接收前端发来的id
   let { id } = req.query;
   // 构造通过id删除数据的sql语句
-  const sqlStr = `delete from user where id = ${id}`;;
+  const sqlStr = `delete from user where id = ${id}`;
   // 执行sql语句
   connection.query(sqlStr, (err, data) => {
-    if(err){
+    if (err) {
       throw err
-    }else{
-      if(data.affectedRows > 0){
-        res.send({"errcode":1,"msg":"删除成功！"})
-      }else{
-        res.send({"errcode":0,"msg":"删除失败！"})
+    } else {
+      if (data.affectedRows > 0) {
+        res.send({ "errcode": 1, "msg": "删除成功！" })
+      } else {
+        res.send({ "errcode": 0, "msg": "删除失败！" })
       }
     }
   })
-})
+});
 
+// 管理列表中的修改数据
+// 接收前端发来的请求，修改一条用户数据
+router.get('/userEdit', (req, res) => {
+  // 接收前端发送的数据，并结构
+  let { id } = req.query;
+  // 构造sql语句，通过id查询数据
+  const sqlStr = `select * from user where id=${id}`;
+  // 执行sql语句
+  connection.query(sqlStr, (err, data) => {
+    if (err) {
+      throw err
+    } else {
+      res.send(data)
+    }
+  })
+});
+
+// 修改页面中修改后的数据
+// 接收前端发来请求，保存数据
+router.get('/userSave', (req, res) => {
+  // 接收前端发来的参数
+  let { username, password, groups, id } = req.query;
+  // 构造sql语句
+  const sqlStr = `update user set username = '${username}',password='${password}',groups='${groups}' where id='${id}'`;
+  // 执行修改的sql语句
+  connection.query(sqlStr, (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      if (data.affectedRows > 0) {
+        res.send({ "errcode": 1, "msg": "修改成功!" })
+      } else {
+        res.send({ "errcode": 0, "msg": "修改失败!" })
+      }
+    }
+  })
+});
+
+// 用户列表中的批量删除功能
+// 接收前端发来的数据，批量删除数据
+router.post('/selectDel', (req, res) => {
+  let idArr = req.body['idArr[]'];
+  // 构造sql语句
+  const sqlStr = `delete from user where id in(${idArr})`
+  // 执行sql语句
+  connection.query(sqlStr, (err, data) => {
+    if (err) {
+      throw err
+    } else {
+      if (data.affectedRows > 0) {
+        res.send({ "errcode": 1, "msg": "批量删除成功!" })
+      } else {
+        res.send({ "errcode": 0, "msg": "批量删除失败!" })
+      }
+    }
+  })
+});
 
 
 module.exports = router;
